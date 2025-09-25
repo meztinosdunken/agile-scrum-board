4a86404b3352699f85e64a25043041d4e2230407
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-25 09:38:37 -0500

    Update welcome message for clarity

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Agile Scrum Board API!');
+    res.send('Welcome to the Agile Scrum Board API! Your collaborative project management tool.');
 });
 
 io.on('connection', (socket) => {
